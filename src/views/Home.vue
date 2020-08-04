a7678dc5567194aee7601466cd95311e4535376e
小月月 <pengyufang520@qq.com>
2020-08-04 21:42:26 +0800

    1，创建项目 2，初始化项目 3，安装并初始化vue-router

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+    <h1>轱辘UI</h1>
+    <h2>一个历害的 UI 框架</h2>
+    <p class="actions">
+    <a>GitHub</a>
+    <a>开始</a>
+    </p>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+  .topnar{border:1px solid red;}
+</style>
