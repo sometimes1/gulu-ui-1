37b23406c817bf90010157752e7c50912e72fc6e
小月月 <pengyufang520@qq.com>
2020-08-05 22:07:40 +0800

    topnav样式更新

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import './index.scss'
 import {createWebHashHistory,createRouter} from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
@@ -10,7 +10,7 @@ const router = createRouter({
   history:history,
   routes:[
     {path:'/',component:Home},
-    {path:'/xxx',component:Doc}
+    {path:'/doc',component:Doc}
 ]
 })
 
