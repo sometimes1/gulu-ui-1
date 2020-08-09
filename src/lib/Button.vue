026d75c37566f785fad9f70c7262c8fbce4c5eeb
小月月 <pengyufang520@qq.com>
2020-08-09 22:04:05 +0800

    创建Button组件

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,22 @@
 <template>
-  <button>
+  <button v-bind="$attrs">
     <slot />
   </button>
 </template>
+<script lang="ts">
+export default {
+  inheritAttrs: false, // 继承属性改为false
+  props: {},
+  setup(props, context) {
+    // es6语法括展操作符
+    // size取出来，其他的都放在rest
+    const { size, ...rest } = context.attrs
+    return { size, rest }
+  },
+}
+</script>
+<style lang="scss" scoped>
+div {
+  border: 1px solid red;
+}
+</style>
