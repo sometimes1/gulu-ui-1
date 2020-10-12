955ebe9641da0ebddc941c03d25a835a357bdcaf
彭玉芳 <191979737@qq.com>
2020-10-12 15:53:39 +0800

    渲染Tabs 内容 和添加css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>Tabs 组件
-    <component :is="defaults[0]" />
-    <component :is="defaults[1]" />
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -10,12 +14,41 @@ export default {
   setup(props, context) {
     const defaults = context.slots.default()
     // console.log(defaults[0].type === Tab)
-    defaults.forEach((tag)=>{
-      if(tag.type !== Tab){
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
         throw new Error('Tabs 子标签必须是 Tab')
       }
     })
-    return { defaults }
+    const titles = defaults.map((tag) => {
+      return tag.props.title
+    })
+    return { defaults, titles }
   }
 }
 </script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
