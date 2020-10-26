c63134cdb1a347e7359da1a03982587e5889cdda
彭玉芳 <191979737@qq.com>
2020-10-26 10:34:49 +0800

    修改Tabs的bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,74 +1,69 @@
 <template>
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav" ref="container">
-      <div class="gulu-tabs-nav-item" :ref="el=>{if (t===selected) selectedItem = el}" @click="select(t)" :class="{selected: t === selected}" v-for="(t,index) in titles" :key="index">{{t}}</div>
+      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
       <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
-      <component class="gulu-tabs-content-item" :is="current" :key="current.props.title" />
+      <component :is="current" :key="current.props.title" />
     </div>
   </div>
 </template>
-<script lang="ts">
+
+<script lang="ts" setup="props, context">
+import Tab from './Tab.vue'
 import {
   computed,
-  HtmlHTMLAttributes,
-  onMounted,
   ref,
-  watchEffect
+  watchEffect,
+  onMounted,
+  SetupContext,
+  Component
 } from 'vue'
-import Tab from './Tab.vue'
+declare const props: { selected: string }
+declare const context: SetupContext
 export default {
   props: {
-    selected: { type: String }
-  },
-  setup(props, context) {
-    // 里面的数组是一个HTMLdiv元素的数组 (ts泛型)
-    const selectedItem = ref<HTMLDivElement>(null)
-    const indicator = ref<HTMLDivElement>(null)
-    const container = ref<HTMLDivElement>(null)
-    onMounted(() => {
-      // watchEffect() 会在挂载之前就执行所以在挂载之后再watchEffect
-      watchEffect(() => {
-        //获取宽高的位置 还有height top
-        const { width } = selectedItem.value.getBoundingClientRect()
-        indicator.value.style.width = width + 'px'
-        //析构赋值的重命名语法 然后相减
-        const { left: left1 } = container.value.getBoundingClientRect()
-        const { left: left2 } = selectedItem.value.getBoundingClientRect()
-        const left = left2 - left1
-        indicator.value.style.left = left + 'px'
-      })
-    })
-    const defaults = context.slots.default()
-    // console.log(defaults[0].type === Tab)
-    defaults.forEach((tag) => {
-      if (tag.type !== Tab) {
-        throw new Error('Tabs 子标签必须是 Tab')
-      }
-    })
-
-    const current = computed(() => {
-      return defaults.find((tag) => tag.props.title === props.selected)
-    })
-    const titles = defaults.map((tag) => {
-      return tag.props.title
-    })
-    const select = (title: string) => {
-      context.emit('update:selected', title)
-    }
-    return {
-      defaults,
-      titles,
-      current,
-      select,
-      selectedItem,
-      indicator,
-      container
+    selected: {
+      type: String
     }
   }
 }
+export const selectedItem = ref<HTMLDivElement>(null)
+export const indicator = ref<HTMLDivElement>(null)
+export const container = ref<HTMLDivElement>(null)
+onMounted(() => {
+  watchEffect(
+    () => {
+      const { width } = selectedItem.value.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+      const { left: left1 } = container.value.getBoundingClientRect()
+      const { left: left2 } = selectedItem.value.getBoundingClientRect()
+      const left = left2 - left1
+      indicator.value.style.left = left + 'px'
+    },
+    {
+      flush: 'post'
+    }
+  )
+})
+export const defaults = context.slots.default()
+defaults.forEach((tag) => {
+  if ((tag.type as Component).name !== Tab.name) {
+    throw new Error('Tabs 子标签必须是 Tab')
+  }
+})
+export const current = computed(() => {
+  return defaults.find((tag) => tag.props.title === props.selected)
+})
+export const titles = defaults.map((tag) => {
+  return tag.props.title
+})
+export const select = (title: string) => {
+  context.emit('update:selected', title)
+}
 </script>
+
 <style lang="scss">
 $blue: #40a9ff;
 $color: #333;
